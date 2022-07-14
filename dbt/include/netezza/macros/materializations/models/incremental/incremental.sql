92c50c6e2d6ec7ddb87a1277de51eb1e365b0ce6
Christian Minich <MINICHC@EMAIL.CHOP.EDU>
2022-07-14 11:15:24 -0400

    add additional incremental boilerplate from dbt-core

--- dbt/include/netezza/macros/materializations/models/incremental/incremental.sql
+++ dbt/include/netezza/macros/materializations/models/incremental/incremental.sql
@@ -37,29 +37,52 @@
 {% materialization incremental, adapter='netezza' -%}
    
   {%- set unique_key = config.get('unique_key') -%}
+
+  {% set target_relation = this.incorporate(type='table') %}
+  {% set existing_relation = load_relation(this) %}
+  {% set tmp_relation = make_temp_relation(target_relation) %}
   {%- set full_refresh_mode = (should_full_refresh()) -%}
 
-  {% set target_relation = this %}
-  {% set existing_relation = load_relation(this) %}
-  {% set tmp_relation = make_temp_relation(this) %}
-
+  {% set on_schema_change = incremental_validate_on_schema_change(config.get('on_schema_change'), default='ignore') %}
   {#-- Validate early so we don't run SQL if the strategy is invalid --#}
   {% set strategy = dbt_netezza_validate_get_incremental_strategy(config) -%}
+  
+  {% set tmp_identifier = model['name'] + '__dbt_tmp' %}
+  {% set backup_identifier = model['name'] + "__dbt_backup" %}
+  -- the intermediate_ and backup_ relations should not already exist in the database; get_relation
+  -- will return None in that case. Otherwise, we get a relation that we can drop
+  -- later, before we try to use this name for the current operation. This has to happen before
+  -- BEGIN, in a separate transaction
+  {% set preexisting_intermediate_relation = adapter.get_relation(identifier=tmp_identifier,
+                                                                  schema=schema,
+                                                                  database=database) %}
+  {% set preexisting_backup_relation = adapter.get_relation(identifier=backup_identifier,
+                                                            schema=schema,
+                                                            database=database) %}
+  {{ drop_relation_if_exists(preexisting_intermediate_relation) }}
+  {{ drop_relation_if_exists(preexisting_backup_relation) }}
+  {{ run_hooks(pre_hooks, inside_transaction=False) }}
 
-  {{ run_hooks(pre_hooks) }}
+  -- `BEGIN` happens here:
+  {{ run_hooks(pre_hooks, inside_transaction=True) }}
+
+  {% set to_drop = [] %}
+  {# -- first check whether we want to full refresh for source view or config reasons #}
+  {% set trigger_full_refresh = (full_refresh_mode or existing_relation.is_view) %}
 
   {% if existing_relation is none %}
     {% set build_sql = create_table_as(False, target_relation, sql) %}
   
-  {% elif existing_relation.is_view %}
-    {#-- Can't overwrite a view with a table - we must drop --#}
-    {{ log("Dropping relation " ~ target_relation ~ " because it is a view and this model is a table.") }}
-    {% do adapter.drop_relation(existing_relation) %}
-    {% set build_sql = create_table_as(False, target_relation, sql) %}
-  
-  {% elif full_refresh_mode %}
-    {% set build_sql = create_table_as(False, target_relation, sql) %}
-  
+  {% elif trigger_full_refresh %}
+    {#-- Make sure the backup doesn't exist so we don't encounter issues with the rename below #}
+    {% set tmp_identifier = model['name'] + '__dbt_tmp' %}
+    {% set backup_identifier = model['name'] + '__dbt_backup' %}
+    {% set intermediate_relation = existing_relation.incorporate(path={"identifier": tmp_identifier}) %}
+    {% set backup_relation = existing_relation.incorporate(path={"identifier": backup_identifier}) %}
+
+    {% set build_sql = create_table_as(False, intermediate_relation, sql) %}
+    {% set need_swap = true %}
+    {% do to_drop.append(backup_relation) %}
   {% else %}
     {% do run_query(create_table_as(True, tmp_relation, sql)) %}
     {% do adapter.expand_target_column_types(
