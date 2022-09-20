463a6483c7e89e9a1cf28785ecde8c05da06323f
Joe Mirizio <mirizioj@chop.edu>
2022-09-20 19:30:55 +0000

    Bump version to 1.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_namespace_packages, setup
 
 package_name = "dbt-netezza"
 # make sure this always matches dbt/adapters/{adapter}/__version__.py
-package_version = "1.1.0"
+package_version = "1.1.1"
 description = """The Netezza adapter plugin for dbt"""
 
 setup(
@@ -16,8 +16,5 @@ setup(
     url="https://github.com/chop-analytics/dbt-netezza",
     packages=find_namespace_packages(include=["dbt", "dbt.*"]),
     include_package_data=True,
-    install_requires=[
-        'dbt-core~=1.1.0',
-        'pyodbc~=4.0'
-    ]
+    install_requires=["dbt-core~=1.1.0", "pyodbc~=4.0"],
 )
