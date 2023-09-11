a6156f784d3190b0ae8fd5f55cc4edbc7395d545
kabatd <kabatd@chop.edu>
2023-09-11 09:38:52 -0400

    Convert privilege_lookup to list of dicts for greater clarity

--- dbt/include/netezza/macros/grants.sql
+++ dbt/include/netezza/macros/grants.sql
@@ -1,30 +1,30 @@
 {% macro netezza__get_show_grant_sql(relation) %}
     {% set privilege_lookup = [
-        ('privilege_list', 0, 'list'), 
-        ('privilege_select', 1, 'select'),  
-        ('privilege_insert', 2, 'insert'), 
-        ('privilege_update', 3, 'update'), 
-        ('privilege_delete', 4, 'delete'), 
-        ('privilege_truncate', 5, 'truncate'),  
-        ('privilege_lock', 6, 'lock'), 
-        ('privilege_alter', 7, 'alter'), 
-        ('privilege_drop', 8, 'drop'), 
-        ('privilege_abort', 9, 'abort'), 
-        ('privilege_load', 10, 'load'), 
-        ('privilege_genstats', 11, 'genstats'),  
-        ('privilege_groom', 12, 'groom'),  
-        ('privilege_execute', 13, 'execute'), 
-        ('privilege_label_access', 14, 'label access'),  
-        ('privilege_label_restrict', 15, 'label restrict'), 
-        ('privilege_label_expand', 16, 'label expand'), 
-        ('privilege_execute_as', 17, 'execute as'), 
+        {'wide_col_name':'privilege_list',           'shift_bits':0,  'privilege_type':'list'},
+        {'wide_col_name':'privilege_select',         'shift_bits':1,  'privilege_type':'select'},
+        {'wide_col_name':'privilege_insert',         'shift_bits':2,  'privilege_type':'insert'},
+        {'wide_col_name':'privilege_update',         'shift_bits':3,  'privilege_type':'update'},
+        {'wide_col_name':'privilege_delete',         'shift_bits':4,  'privilege_type':'delete'},
+        {'wide_col_name':'privilege_truncate',       'shift_bits':5,  'privilege_type':'truncate'},
+        {'wide_col_name':'privilege_lock',           'shift_bits':6,  'privilege_type':'lock'},
+        {'wide_col_name':'privilege_alter',          'shift_bits':7,  'privilege_type':'alter'},
+        {'wide_col_name':'privilege_drop',           'shift_bits':8,  'privilege_type':'drop'},
+        {'wide_col_name':'privilege_abort',          'shift_bits':9,  'privilege_type':'abort'},
+        {'wide_col_name':'privilege_load',           'shift_bits':10, 'privilege_type':'load'},
+        {'wide_col_name':'privilege_genstats',       'shift_bits':11, 'privilege_type':'genstats'},
+        {'wide_col_name':'privilege_groom',          'shift_bits':12, 'privilege_type':'groom'},
+        {'wide_col_name':'privilege_execute',        'shift_bits':13, 'privilege_type':'execute'},
+        {'wide_col_name':'privilege_label_access',   'shift_bits':14, 'privilege_type':'label access'},
+        {'wide_col_name':'privilege_label_restrict', 'shift_bits':15, 'privilege_type':'label restrict'},
+        {'wide_col_name':'privilege_label_expand',   'shift_bits':16, 'privilege_type':'label expand'},
+        {'wide_col_name':'privilege_execute_as',     'shift_bits':17, 'privilege_type':'execute as'},
     ] %}
 
     with parse_bitwise as (
         select
             username as grantee,
-            {% for wide_col_name, shift_bits, _ in privilege_lookup %}
-            case when int8and(uopobjpriv, 1<<{{ shift_bits }}) = 0 then 0 else 1 end as {{ wide_col_name }}
+            {% for record in privilege_lookup %}
+            case when int8and(uopobjpriv, 1<<{{ record['shift_bits'] }}) = 0 then 0 else 1 end as {{ record['wide_col_name'] }}
             {%- if not loop.last %},{% endif -%}
             {% endfor %}
         from
@@ -36,8 +36,8 @@
     ),
 
     privileges_pivot as (
-        {% for wide_col_name, _, privilege_type in privilege_lookup %}
-        select grantee, case when {{ wide_col_name }} = 1 then '{{ privilege_type }}' end as privilege_type from parse_bitwise
+        {% for record in privilege_lookup %}
+        select grantee, case when {{ record['wide_col_name'] }} = 1 then '{{ record["privilege_type"] }}' end as privilege_type from parse_bitwise
         {%- if not loop.last %}
         union all
         {% endif -%}
