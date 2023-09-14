358385f70b86fcecea723971abe034191014975e
Joe Mirizio <mirizioj@chop.edu>
2023-09-14 15:42:50 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,84 @@
 #!/usr/bin/env python
-from setuptools import find_namespace_packages, setup
+import os
+import sys
+import re
+
+# require python 3.8 or newer
+if sys.version_info < (3, 8):
+    print("Error: dbt does not support this version of Python.")
+    print("Please upgrade to Python 3.8 or higher.")
+    sys.exit(1)
+
+
+# require version of setuptools that supports find_namespace_packages
+from setuptools import setup
+
+try:
+    from setuptools import find_namespace_packages
+except ImportError:
+    # the user has a downlevel version of setuptools.
+    print("Error: dbt requires setuptools v40.1.0 or higher.")
+    print(
+        'Please upgrade setuptools with "pip install --upgrade setuptools" '
+        "and try again"
+    )
+    sys.exit(1)
+
+
+# pull long description from README
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md")) as f:
+    long_description = f.read()
+
+
+# get this package's version from dbt/adapters/<name>/__version__.py
+def _get_plugin_version_dict():
+    _version_path = os.path.join(
+        this_directory, "dbt", "adapters", "netezza", "__version__.py"
+    )
+    _semver = r"""(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)"""
+    _pre = r"""((?P<prekind>a|b|rc)(?P<pre>\d+))?"""
+    _version_pattern = rf"""version\s*=\s*["']{_semver}{_pre}["']"""
+    with open(_version_path) as f:
+        match = re.search(_version_pattern, f.read().strip())
+        if match is None:
+            raise ValueError(f"invalid version at {_version_path}")
+        return match.groupdict()
+
+
+# require a compatible minor version (~=), prerelease if this is a prerelease
+def _get_dbt_core_version():
+    parts = _get_plugin_version_dict()
+    minor = "{major}.{minor}.0".format(**parts)
+    pre = parts["prekind"] + "1" if parts["prekind"] else ""
+    return f"{minor}{pre}"
+
 
 package_name = "dbt-netezza"
-# make sure this always matches dbt/adapters/{adapter}/__version__.py
 package_version = "1.2.0"
+dbt_core_version = _get_dbt_core_version()
 description = """The Netezza adapter plugin for dbt"""
 
 setup(
     name=package_name,
     version=package_version,
     description=description,
-    long_description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Joe Mirizio",
     author_email="mirizioj@chop.edu",
     url="https://github.com/chop-analytics/dbt-netezza",
     packages=find_namespace_packages(include=["dbt", "dbt.*"]),
     include_package_data=True,
-    install_requires=["dbt-core~=1.2", "pyodbc~=4.0"],
+    install_requires=["dbt-core~={}".format(dbt_core_version), "pyodbc~=4.0"],
+    zip_safe=False,
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.8",
 )
