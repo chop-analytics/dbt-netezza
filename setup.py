d77d79cadaf79bb6da8b3bb4603e21297fe2d5fa
Joe Mirizio <mirizioj@chop.edu>
2023-09-22 15:28:22 -0400

    Bump version to 1.2.1

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ def _get_dbt_core_version():
 
 
 package_name = "dbt-netezza"
-package_version = "1.2.0"
+package_version = "1.2.1"
 dbt_core_version = _get_dbt_core_version()
 description = """The Netezza adapter plugin for dbt"""
 
