d59d710e22ae1f9cac197b95839a9f27b9be69dd
kabatd <kabatd@chop.edu>
2023-11-07 14:03:20 +0000

    Get TestModelConstraintsRuntimeEnforcementNetezza to pass

--- tests/functional/adapter/constraint_fixtures.py
+++ tests/functional/adapter/constraint_fixtures.py
@@ -45,7 +45,33 @@ insert into <model_identifier>
     ) as model_subq;
     """
 
-test_base__constraints_runtime_ddl_enforcement__expected_sql = """
+test_base_model_constraints_runtime_enforcement__expected_sql = """
+create table <model_identifier> (
+    id integer not null,
+    color varchar(2000),
+    date_day varchar(2000),
+    check ((id > 0)),
+    check (id >= 1),
+    primary key (id),
+    constraint strange_uniqueness_requirement unique (color, date_day),
+    foreign key (id) references <foreign_key_model_identifier> (id)
+) ;
+insert into <model_identifier> 
+    select
+       id,
+       color,
+       date_day
+       from
+    (
+        -- depends_on: <foreign_key_model_identifier>
+        select
+            'blue' as color,
+            1 as id,
+            '2019-01-01' as date_day
+    ) as model_subq;
+"""
+
+test_base_constraints_runtime_ddl_enforcement__expected_sql = """
 create table <model_identifier> (
     id integer not null primary key check ((id > 0)) check (id >= 1) references <foreign_key_model_identifier> (id) unique,
     color varchar(2000),
@@ -65,6 +91,22 @@ insert into <model_identifier>
             '2019-01-01' as date_day
     ) as model_subq;
 """
+
+
+my_model_incremental_contract_sql_header_sql = """
+{{
+  config(
+    materialized = "incremental",
+    on_schema_change="append_new_columns"
+  )
+}}
+
+{% call set_sql_header(config) %}
+set time zone 'Asia/Kolkata';
+{%- endcall %}
+select current_timestamp::timetz as current_time_w_tz
+"""
+
 
 model_schema_yml = """
 version: 2
@@ -227,6 +269,62 @@ models:
           - type: primary_key
           - type: check
             expression: (id > 0)
+        tests:
+          - unique
+      - name: color
+        data_type: varchar(2000)
+      - name: date_day
+        data_type: varchar(2000)
+  - name: foreign_key_model
+    config:
+      contract:
+        enforced: true
+    columns:
+      - name: id
+        data_type: integer
+        constraints:
+          - type: unique
+          - type: primary_key
+"""
+
+model_contract_header_schema_yml = """
+version: 2
+models:
+  - name: my_model_contract_sql_header
+    config:
+      contract:
+        enforced: true
+    columns:
+      - name: column_name
+        data_type: varchar(2000)
+"""
+
+constrained_model_schema_yml = """
+version: 2
+models:
+  - name: my_model
+    config:
+      contract:
+        enforced: true
+    constraints:
+      - type: check
+        expression: (id > 0)
+      - type: check
+        expression: id >= 1
+      - type: primary_key
+        columns: [ id ]
+      - type: unique
+        columns: [ color, date_day ]
+        name: strange_uniqueness_requirement
+      - type: foreign_key
+        columns: [ id ]
+        expression: {schema}.foreign_key_model (id)
+    columns:
+      - name: id
+        data_type: integer
+        description: hello
+        constraints:
+          - type: not_null
         tests:
           - unique
       - name: color
--- tests/functional/adapter/test_constraints.py
+++ tests/functional/adapter/test_constraints.py
@@ -14,6 +14,14 @@ from dbt.tests.adapter.constraints.test_constraints import (
     TestIncrementalForeignKeyConstraint as BaseIncrementalForeignKeyConstraint,
 )
 
+from dbt.tests.util import (
+    run_sql_with_adapter,
+    get_manifest,
+    run_dbt_and_capture,
+    write_file,
+    relation_from_name
+)
+
 from dbt.tests.adapter.constraints.fixtures import (
     my_model_with_quoted_column_name_sql,
     my_model_incremental_wrong_order_sql,
@@ -21,15 +29,34 @@ from dbt.tests.adapter.constraints.fixtures import (
     my_model_wrong_order_depends_on_fk_sql,
     foreign_key_model_sql,
     my_incremental_model_sql,
+    my_model_data_type_sql,
+    model_data_type_schema_yml,
 )
 from tests.functional.adapter.constraint_fixtures import (
     model_quoted_column_schema_yml,
     test_constraint_quoted_column_netezza__expected_sql,
     model_schema_yml,
     model_fk_constraint_schema_yml,
-    test_base__constraints_runtime_ddl_enforcement__expected_sql,
+    test_base_constraints_runtime_ddl_enforcement__expected_sql,
+    model_contract_header_schema_yml,
+    my_model_incremental_contract_sql_header_sql,
+    test_base_model_constraints_runtime_enforcement__expected_sql,
+    constrained_model_schema_yml,
 )
 import pytest
+
+
+class TestConstraintQuotedColumnNetezza(BaseConstraintQuotedColumn):
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "my_model.sql": my_model_with_quoted_column_name_sql,
+            "constraints_schema.yml": model_quoted_column_schema_yml,
+        }
+
+    @pytest.fixture(scope="class")
+    def expected_sql(self):
+            return test_constraint_quoted_column_netezza__expected_sql
 
 
 class BaseConstraintsColumnsEqualNetezza(BaseConstraintsColumnsEqual):
@@ -44,7 +71,7 @@ class BaseConstraintsColumnsEqualNetezza(BaseConstraintsColumnsEqual):
         # sql_column_value, schema_data_type, error_data_type
         return [
             ["1", schema_int_type, int_type],
-            # ["'1'", string_type, string_type],
+            ["'1'", string_type, string_type],
             # ["true", "boolean", "BOOL"],
             # ["'2013-11-03 00:00:00-07'::timestamptz", "timestamptz", "DATETIMETZ"],
             # ["'2013-11-03 00:00:00-07'::timestamp", "timestamp", "DATETIME"],
@@ -52,6 +79,54 @@ class BaseConstraintsColumnsEqualNetezza(BaseConstraintsColumnsEqual):
             # ["ARRAY[1,2,3]", "int[]", "INTEGERARRAY"],
             # ["'1'::numeric", "numeric", "DECIMAL"],
         ]
+
+    # need to overwrite: test creates a series of models parameterized with a data
+    # type; if these are not dropped in between iterations, Netezza will complain about
+    # writing different data types into an existing table
+    def test__constraints_wrong_column_data_types(
+        self, project, string_type, int_type, schema_string_type, schema_int_type, data_types
+    ):
+        for (sql_column_value, schema_data_type, error_data_type) in data_types:
+            # Write parametrized data_type to sql file
+            write_file(
+                my_model_data_type_sql.format(sql_value=sql_column_value),
+                "models",
+                "my_model_data_type.sql",
+            )
+
+            # Write wrong data_type to corresponding schema file
+            # Write integer type for all schema yaml values except when testing integer type itself
+            wrong_schema_data_type = (
+                schema_int_type
+                if schema_data_type.upper() != schema_int_type.upper()
+                else schema_string_type
+            )
+            wrong_schema_error_data_type = (
+                int_type if schema_data_type.upper() != schema_int_type.upper() else string_type
+            )
+            write_file(
+                model_data_type_schema_yml.format(data_type=wrong_schema_data_type),
+                "models",
+                "constraints_schema.yml",
+            )
+
+            results, log_output = run_dbt_and_capture(
+                ["run", "-s", "my_model_data_type"], expect_pass=False
+            )
+            manifest = get_manifest(project.project_root)
+            model_id = "model.test.my_model_data_type"
+            my_model_config = manifest.nodes[model_id].config
+            contract_actual_config = my_model_config.contract
+
+            assert contract_actual_config.enforced is True
+            expected = [
+                "wrong_data_type_column_name",
+                error_data_type,
+                wrong_schema_error_data_type,
+                "data type mismatch",
+            ]
+            # run_sql_with_adapter(project.adapter, f"drop table my_model_data_type")
+            assert all([(exp in log_output or exp.upper() in log_output) for exp in expected])
 
 class BaseIncrementalConstraintsColumnsEqualNetezza(BaseConstraintsColumnsEqualNetezza):
     @pytest.fixture(scope="class")
@@ -61,19 +136,6 @@ class BaseIncrementalConstraintsColumnsEqualNetezza(BaseConstraintsColumnsEqualN
             "my_model_wrong_name.sql": my_model_incremental_wrong_name_sql,
             "constraints_schema.yml": model_schema_yml,
         }
-
-
-class TestConstraintQuotedColumnNetezza(BaseConstraintQuotedColumn):
-    @pytest.fixture(scope="class")
-    def models(self):
-        return {
-            "my_model.sql": my_model_with_quoted_column_name_sql,
-            "constraints_schema.yml": model_quoted_column_schema_yml,
-        }
-
-    @pytest.fixture(scope="class")
-    def expected_sql(self):
-            return test_constraint_quoted_column_netezza__expected_sql
 
 
 class TestIncrementalConstraintsColumnsEqualNetezza(
@@ -110,15 +172,22 @@ class BaseConstraintsRuntimeDdlEnforcementNetezza(BaseConstraintsRuntimeDdlEnfor
 
     @pytest.fixture(scope="class")
     def expected_sql(self):
-        return test_base__constraints_runtime_ddl_enforcement__expected_sql
+        return test_base_constraints_runtime_ddl_enforcement__expected_sql
 
 class TestIncrementalConstraintsRuntimeDdlEnforcementNetezza(
     BaseConstraintsRuntimeDdlEnforcementNetezza
 ):
     pass
 
+class BaseIncrementalContractSqlHeaderNetezza(BaseIncrementalContractSqlHeader):
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "my_model_contract_sql_header.sql": my_model_incremental_contract_sql_header_sql,
+            "constraints_schema.yml": model_contract_header_schema_yml,
+        }
 
-class TestIncrementalContractSqlHeaderNetezza(BaseIncrementalContractSqlHeader):
+class TestIncrementalContractSqlHeaderNetezza(BaseIncrementalContractSqlHeaderNetezza):
     pass
 
 
@@ -126,8 +195,21 @@ class TestIncrementalForeignKeyConstraintNetezza(BaseIncrementalForeignKeyConstr
     pass
 
 
+class BaseModelConstraintsRuntimeEnforcementNetezza(BaseModelConstraintsRuntimeEnforcement):
+    @pytest.fixture(scope="class")
+    def expected_sql(self):
+        return test_base_model_constraints_runtime_enforcement__expected_sql
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "my_model.sql": my_model_wrong_order_depends_on_fk_sql,
+            "foreign_key_model.sql": foreign_key_model_sql,
+            "constraints_schema.yml": constrained_model_schema_yml,
+        }
+
 class TestModelConstraintsRuntimeEnforcementNetezza(
-    BaseModelConstraintsRuntimeEnforcement
+    BaseModelConstraintsRuntimeEnforcementNetezza
 ):
     pass
 
